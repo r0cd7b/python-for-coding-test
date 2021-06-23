18ad54e8ba6dd79d6ef984376eabde2e5801dd07
r0cd7b <tkdrbs1928@naver.com>
2021-06-23 21:01:01 +0900

    3-2.py was added.

--- 3-1.py
+++ 3-1.py
@@ -1,15 +1,14 @@
 # 거스름돈
 import time
 
+n = 1260
+
 start_time = time.time()
 
-n = 1260
 total_coins = 0
-
 for coin in [500, 100, 50, 10]:
     total_coins += n // coin
     n %= coin
-
 print(total_coins)
 
 print(f"time: {time.time() - start_time}")
