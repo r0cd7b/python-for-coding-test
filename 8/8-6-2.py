817e8d8b35696f0812e3d405410c484d5158a033
r0cd7b <tkdrbs1928@naver.com>
2021-07-14 14:11:52 +0900

    8-6-2.py was corrected.

--- 8/8-6-2.py
+++ 8/8-6-2.py
@@ -10,7 +10,7 @@ n = int(input_1)
 array = list(map(int, input_2.split()))
 array[1] = max(array[0], array[1])
 for i in range(2, n):
-    array[i] = max(array[i - 1], array[i - 2] + array[i])
+    array[i] = max(array[i - 1], array[i] + array[i - 2])
 print(array[-1])
 
 print(f"time: {time.time() - start_time}")
