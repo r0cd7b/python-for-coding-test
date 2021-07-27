faa441d2e799e949e13f1017c42e525624bb6f17
r0cd7b <tkdrbs1928@naver.com>
2021-07-27 13:01:45 +0900

    "9-4.py" has been modified.

--- 9/9-4.py
+++ 9/9-4.py
@@ -1,14 +1,19 @@
 # 미래 도시
 """
-4
-7
-1 2 4
-1 4 6
-2 1 3
-2 3 7
-3 1 5
-3 4 4
-4 3 2
+5 7
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5
+
+4 2
+1 3
+2 4
+3 4
 """
 import sys
 import time
@@ -19,19 +24,22 @@ INF = int(1e9)
 graph = [[INF] * n for _ in range(n)]
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a - 1][b - 1] = 1
+    a, b = a - 1, b - 1
+    graph[a][b], graph[b][a] = 1, 1
 x, k = map(int, input().split())
-print(x, k)
+
+start_time = time.time()
+
 for a in range(n):
     for b in range(n):
         if a == b:
             graph[a][b] = 0
-for k in range(n):
+for c in range(n):
     for a in range(n):
         for b in range(n):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-print(x, k)
-
-start_time = time.time()
+            graph[a][b] = min(graph[a][b], graph[a][c] + graph[c][b])
+k -= 1
+distance = graph[0][k] + graph[k][x - 1]
+print(-1 if distance >= INF else distance)
 
 print(f"time: {time.time() - start_time}")
