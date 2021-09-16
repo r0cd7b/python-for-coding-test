a4a2dd544851e1d53a2fddf0375e57107833b47b
r0cd7b <tkdrbs1928@naver.com>
2021-09-16 18:19:07 +0900

    "A12.py" has been modified.

--- 12/A12.py
+++ 12/A12.py
@@ -1,11 +1,12 @@
 # 기둥과 보 설치
 def solution(n, build_frame):
-    n += 5
-    wall = [[0b00] * n for _ in range(n)]
+    n += 1
+    wall = [[0b00] * (n + 2) for _ in range(n + 3)]
     pillar, beam = 0b01, 0b10
+    floor = 1
 
     for x, y, a, b in build_frame:
-        x, y = x + 2, y + 2
+        x, y = x + 1, y + 1
         if a:
             if b:
                 if wall[x][y - 1] & pillar or \
@@ -23,7 +24,7 @@ def solution(n, build_frame):
                     wall[x][y] ^= beam
         else:
             if b:
-                if y == 2 or \
+                if y == floor or \
                         wall[x][y] & beam or \
                         wall[x - 1][y] & beam or \
                         wall[x][y - 1] & pillar:
@@ -41,12 +42,12 @@ def solution(n, build_frame):
                     wall[x][y] ^= pillar
 
     answer = []
-    for x in range(2, n - 2):
-        for y in range(2, n - 2):
+    for x in range(1, n + 1):
+        for y in range(1, n + 1):
             if wall[x][y] & pillar:
-                answer.append([x - 2, y - 2, 0])
+                answer.append([x - 1, y - 1, 0])
             if wall[x][y] & beam:
-                answer.append([x - 2, y - 2, 1])
+                answer.append([x - 1, y - 1, 1])
     return answer
 
 
