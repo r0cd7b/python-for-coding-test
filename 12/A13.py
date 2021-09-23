73c1f072bc4d8b9b0eb8c78128f10a1521858331
r0cd7b <tkdrbs1928@naver.com>
2021-09-23 18:30:56 +0900

    "A13.py" has been modified.

--- 12/A13.py
+++ 12/A13.py
@@ -3,27 +3,27 @@ from sys import stdin
 from itertools import combinations
 
 n, m = map(int, stdin.readline().split())
-houses, chicken_houses = [], []
+chicken, houses = [], []
 for r in range(n):
-    buildings = list(map(int, stdin.readline().split()))
+    data = list(map(int, stdin.readline().split()))
     for c in range(n):
-        if buildings[c] == 1:
+        if data[c] == 1:
             houses.append((r, c))
-        elif buildings[c] == 2:
-            chicken_houses.append((r, c))
+        elif data[c] == 2:
+            chicken.append((r, c))
 
-minimum_sum = 1e9
-for number_draw in range(1, m + 1):
-    for chicken_houses_combination in combinations(chicken_houses, number_draw):
-        distance_sum = 0
-        for r1, c1 in houses:
-            minimum_distance = 1e9
-            for r2, c2 in chicken_houses_combination:
-                minimum_distance = min(minimum_distance, abs(r1 - r2) + abs(c1 - c2))
-            distance_sum += minimum_distance
-        minimum_sum = min(minimum_sum, distance_sum)
+distances = 1e9
+for number in range(1, m + 1):
+    for candidates in combinations(chicken, number):
+        result = 0
+        for hx, hy in houses:
+            temp = 1e9
+            for cx, cy in candidates:
+                temp = min(temp, abs(hx - cx) + abs(hy - cy))
+            result += temp
+        distances = min(distances, result)
 
-print(minimum_sum)
+print(distances)
 
 '''
 입력 예시 1
