7d147078518cbb07554f98c2b2998ee18dcfe021
이상균 <tkdrbs1928@naver.com>
2021-10-28 20:42:30 +0900

    "A14.py" has been modified.

--- 12/A14.py
+++ 12/A14.py
@@ -3,18 +3,18 @@ def solution(n, weak, dist):
     if n <= 2:
         return 1
 
-    dist_between_two_weaks = [(weak[i + 1] - weak[i], weak[i]) for i in range(len(weak) - 1)]
-    dist_between_two_weaks.append((n - weak[-1] + weak[0], weak[-1]))
-    dist_between_two_weaks.sort(reverse=True)
-    print(dist_between_two_weaks)
+    wall_condition = [0] * n
+    for i in weak:
+        wall_condition[i] = 1
+    print(wall_condition)
 
-    for number_of_people in range(len(dist)):
-        pass
+    two_weaks = [((weak[(i + 1) % len(weak)] - weak[i]) % n, i) for i in range(len(weak))]
+    print(two_weaks)
+    two_weaks.sort(reverse=True)
+    print(two_weaks)
 
     return -1
 
 
-print(f"{solution(12, [1, 5, 6, 10], [1, 2, 3, 4])} == 2")
+print(f"{solution(12, [1, 5, 6, 10], [1, 2, 3, 4])} == 2\n")
 print(f"{solution(12, [1, 3, 4, 9, 10], [3, 5, 7])} == 1")
-
-print(f"{solution(3, [0, 1, 2], [3, 5, 7])} == 1")
