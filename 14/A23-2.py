5c47a273e3377dc21199ce578a1520c557d4b7e5
이상균 <tkdrbs1928@naver.com>
2021-12-23 21:04:07 +0900

    Commit.

---
+++ 14/A23-2.py
@@ -0,0 +1,42 @@
+# 국영수
+from sys import stdin
+
+n, students = int(stdin.readline()), []
+for _ in range(n):
+    name, korean, english, mathematics = stdin.readline().split()
+    students.append((-int(korean), int(english), -int(mathematics), name))
+
+students.sort()
+
+for _, _, _, name in students:
+    print(name)
+
+"""
+입력 예시
+12
+Junkyu 50 60 100
+Sangkeun 80 60 50
+Sungyoung 80 70 100
+Soong 50 60 90
+Haebin 50 60 100
+Kangsoo 60 80 100
+Donghyuk 80 60 100
+Sei 70 70 70
+Wonseob 70 70 90
+Sanghyun 70 70 80
+nsj 80 80 80
+Taewhan 50 60 90
+출력 예시
+Donghyuk
+Sangkeun
+Sungyoung
+nsj
+Wonseob
+Sanghyun
+Sei
+Kangsoo
+Haebin
+Junkyu
+Soong
+Taewhan
+"""
