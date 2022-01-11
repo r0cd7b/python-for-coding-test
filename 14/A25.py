b1b27e076b624491d46b60b4d565897df3bf1548
이상균 <tkdrbs1928@naver.com>
2022-01-11 17:35:36 +0900

    Commit.

--- 14/A25.py
+++ 14/A25.py
@@ -1,14 +1,14 @@
 # 실패율
 def solution(N, stages):
     stages.sort()
-    index, rate = 0, []
+    index, rates = 0, []
     for i in range(1, N + 1):
         previous = index
         while len(stages) > index and i >= stages[index]:
             index += 1
-        rate.append((-((index - previous) / (len(stages) - previous)), i))
-    rate.sort()
-    return [i[1] for i in rate]
+        rates.append((-((index - previous) / (len(stages) - previous)), i))
+    rates.sort()
+    return [rate[1] for rate in rates]
 
 
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))  # [3, 4, 2, 1, 5]
