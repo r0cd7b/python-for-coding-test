82ebb4bc52b8d5a7ea9cc2d7907552196cad27e7
이상균 <tkdrbs1928@naver.com>
2022-01-12 19:21:54 +0900

    Commit.

--- 14/A26.py
+++ 14/A26.py
@@ -6,14 +6,11 @@ n, bundles = int(stdin.readline()), []
 for _ in range(n):
     heapq.heappush(bundles, int(stdin.readline()))
 
-if n == 1:
-    number = heapq.heappop(bundles)
-else:
-    number = 0
-    for _ in range(n - 1):
-        bundle = heapq.heappop(bundles) + heapq.heappop(bundles)
-        heapq.heappush(bundles, bundle)
-        number += bundle
+number = 0
+while len(bundles) >= 2:
+    bundle = heapq.heappop(bundles) + heapq.heappop(bundles)
+    heapq.heappush(bundles, bundle)
+    number += bundle
 
 print(number)
 
@@ -28,53 +25,10 @@ print(number)
 
 입력 예시
 4
-3
-3
-3
-3
-출력 예시
-24
-
-입력 예시
-4
-30
-40
-50
-100
-출력 예시
-410
-
-입력 예시
-4
-30
-40
-50
-60
-출력 예시
-360
-
-입력 예시
+10
 20
-224
-653
-518
-407
-195
-811
-821
-589
-678
-738
-530
-301
-714
-304
-588
-669
-219
-318
-442
-866
+40
+50
 출력 예시
-44722
+220
 """
