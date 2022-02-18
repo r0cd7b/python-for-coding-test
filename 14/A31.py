3c53b6b907c2d38b05f9d111e5cee96ecebd2173
이상균 <tkdrbs1928@naver.com>
2022-02-18 21:01:44 +0900

    Commit.

--- 14/A31.py
+++ 14/A31.py
@@ -1,23 +1,22 @@
 # 금광
 from sys import stdin
 
-t, mines = int(stdin.readline()), []
-for _ in range(t):
-    (n, m), golds = map(int, stdin.readline().split()), list(map(int, stdin.readline().split()))
-    mines.append([golds[i * m:i * m + m] for i in range(n)])
+sizes = []
+for _ in range(int(stdin.readline())):
+    (n, m), array = map(int, stdin.readline().split()), list(map(int, stdin.readline().split()))
 
-maximum = []
-for mine in mines:
-    for i in range(1, len(mine[0])):
-        for j in range(len(mine)):
-            mine[j][i] += max(mine[j - 1][i - 1] if j >= 1 else 0, mine[j][i - 1],
-                              mine[j + 1][i - 1] if j < len(mine) - 1 else 0)
-    size = mine[0][-1]
-    for i in range(1, len(mine)):
-        size = max(mine[i][-1], size)
-    maximum.append(size)
+    for i in range(1, m):
+        for j in range(n):
+            left, up, down = i - 1, j - 1, j + 1
+            array[j * m + i] += max(array[j * m + left], array[up * m + left] if up >= 0 else 0,
+                                    array[down * m + left] if down < n - 1 else 0)
+    m_max = m - 1
+    result = array[m_max]
+    for i in range(1, n):
+        result = max(array[i * m + m_max], result)
+    sizes.append(result)
 
-for size in maximum:
+for size in sizes:
     print(size)
 
 """
@@ -27,4 +26,7 @@ for size in maximum:
 1 3 3 2 2 1 4 1 0 6 4 7
 4 4
 1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2
+출력 예시
+19
+16
 """
