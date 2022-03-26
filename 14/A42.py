97f1d0b7fedec26d4455148659a6bc6b1229478c
이상균 <tkdrbs1928@naver.com>
2022-03-26 21:21:17 +0900

    Commit.

--- 14/A42.py
+++ 14/A42.py
@@ -5,18 +5,16 @@ from sys import stdin
 def find_parent(parent_, x):
     while parent_[x] != parent_[parent_[x]]:
         parent_[x] = parent_[parent_[x]]
+    return parent_[x]
 
 
 G, P = int(stdin.readline()), int(stdin.readline())
 g, parent = [int(stdin.readline()) for _ in range(P)], [i for i in range(G + 1)]
 for i in range(P):
-    find_parent(parent, g[i])
-    if not parent[g[i]]:
+    if not find_parent(parent, g[i]):
         print(i)
         break
-    previous = parent[g[i]] - 1
-    find_parent(parent, previous)
-    parent[g[i]] = parent[previous]
+    parent[g[i]] = find_parent(parent, parent[g[i]] - 1)
 else:
     print(P)
 
