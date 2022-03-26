9468d8b03f6d5d16b65bd26a309cdd0bf5a53818
이상균 <tkdrbs1928@naver.com>
2022-03-26 21:10:29 +0900

    Commit.

--- 14/A42.py
+++ 14/A42.py
@@ -1,22 +1,44 @@
 # 탑승구
 from sys import stdin
 
+
+def find_parent(parent_, x):
+    while parent_[x] != parent_[parent_[x]]:
+        parent_[x] = parent_[parent_[x]]
+
+
 G, P = int(stdin.readline()), int(stdin.readline())
-g, conditions = [int(stdin.readline()) - 1 for _ in range(P)], [0] * G
-for i in range(1, P + 1):
-    if conditions.count(i - 1) < 2:
+g, parent = [int(stdin.readline()) for _ in range(P)], [i for i in range(G + 1)]
+for i in range(P):
+    find_parent(parent, g[i])
+    if not parent[g[i]]:
         print(i)
         break
-    for j in range(g[i] + 1):
-        conditions[j] = i
+    previous = parent[g[i]] - 1
+    find_parent(parent, previous)
+    parent[g[i]] = parent[previous]
+else:
+    print(P)
 
 """
-입력 예시
+입력 예시 1
 4
 3
 4
 1
 1
-출력 예시
+출력 예시 1
 2
+
+입력 예시 2
+4
+6
+2
+2
+3
+3
+4
+4
+출력 예시 2
+3
 """
