f694320a3d38600d5625a4fa4eea69ec41019552
이상균 <tkdrbs1928@naver.com>
2022-03-31 20:33:06 +0900

    Commit.

--- 14/A44.py
+++ 14/A44.py
@@ -1,22 +1,11 @@
 # 행성 터널
 from sys import stdin
 
-
-def find_parent(x):
-    while parent[x] != parent[parent[x]]:
-        parent[x] = parent[parent[x]]
-    return parent[x]
-
-
-n = int(stdin.readline())
-planets = [tuple(map(int, stdin.readline().split())) for _ in range(n)]
-tunnels, parent, minimum = sorted([(min(abs(planets[i][0] - planets[j][0]), abs(planets[i][1] - planets[j][1]),
-                                        abs(planets[i][2] - planets[j][2])), i, j) for i in range(n - 1) for j in
-                                   range(i + 1, n)]), [i for i in range(n)], 0
-for cost, a, b in tunnels:
-    if find_parent(a) != find_parent(b):
-        parent[parent[b]], minimum = parent[a], minimum + cost
-print(minimum)
+n, planets = int(stdin.readline()), []
+for i in range(n):
+    x, y, z = map(int, stdin.readline().split())
+    planets.append((i, x, y, z))
+print(planets)
 
 """
 입력 예시
