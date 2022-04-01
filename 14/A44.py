f31f796726600439ddf4f87a253639c34b7f630b
이상균 <tkdrbs1928@naver.com>
2022-04-01 13:49:53 +0900

    Commit.

--- 14/A44.py
+++ 14/A44.py
@@ -1,26 +1,21 @@
 # 행성 터널
 from sys import stdin
 
-
-def find_parent(x_):
-    while parent[x_] != parent[parent[x_]]:
-        parent[x_] = parent[parent[x_]]
-    return parent[x_]
-
-
-n, planets = int(stdin.readline()), []
+n, planets, edges, result = int(stdin.readline()), [], [], 0
 for i in range(n):
     x, y, z = map(int, stdin.readline().split())
     planets.append((i, x, y, z))
-edges = []
 for i in range(1, 4):
     planets.sort(key=lambda p: p[i])
     for j in range(n - 1):
-        next_ = j + 1
-        edges.append((abs(planets[j][i] - planets[next_][i]), planets[j][0], planets[next_][0]))
-parent, result = [i for i in range(n)], 0
+        edges.append((planets[j + 1][i] - planets[j][i], planets[j][0], planets[j + 1][0]))
+parent = [i for i in range(n)]
 for cost, a, b in sorted(edges):
-    if find_parent(a) != find_parent(b):
+    while parent[a] != parent[parent[a]]:
+        parent[a] = parent[parent[a]]
+    while parent[b] != parent[parent[b]]:
+        parent[b] = parent[parent[b]]
+    if parent[a] != parent[b]:
         result, parent[parent[b]] = result + cost, parent[a]
 print(result)
 
