7b29b7b7ed9238092d70fa7e805592fcf3025c1c
이상균 <tkdrbs1928@naver.com>
2022-04-02 18:00:34 +0900

    Commit.

--- 14/A45.py
+++ 14/A45.py
@@ -3,15 +3,15 @@ from sys import stdin
 
 for _ in range(int(stdin.readline())):
     n = int(stdin.readline())
-    graph, t = [[] for _ in range(n)], [int(s) - 1 for s in stdin.readline().split()]
+    graph, t = [[False] * n for _ in range(n)], list(map(int, stdin.readline().split()))
     for i in range(n):
         for j in range(i + 1, n):
-            graph[t[i]].append(t[j])
-    degree = [i for i in range(n)]
+            graph[t[i]][t[j]] = True
     for _ in range(int(stdin.readline())):
         a, b = map(int, stdin.readline().split())
         a, b = a - 1, b - 1
         graph[a][b], graph[b][a] = True, False
+    degree = []
 
 """
 입력 예시
