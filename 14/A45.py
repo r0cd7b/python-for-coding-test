c240158fe524db6ac9ec101d5780f9cac14577ec
이상균 <tkdrbs1928@naver.com>
2022-04-04 18:30:04 +0900

    Commit.

--- 14/A45.py
+++ 14/A45.py
@@ -1,17 +1,55 @@
 # 최종 순위
 from sys import stdin
+from collections import deque
 
+results = []
 for _ in range(int(stdin.readline())):
-    n = int(stdin.readline())
-    graph, t = [[False] * n for _ in range(n)], list(map(int, stdin.readline().split()))
+    n, t = int(stdin.readline()), [int(s) - 1 for s in stdin.readline().split()]
+
+    entries = [0] * n
+    for i in range(n):
+        entries[t[i]] = i
+
+    graph = [[False] * n for _ in range(n)]
     for i in range(n):
         for j in range(i + 1, n):
             graph[t[i]][t[j]] = True
+
     for _ in range(int(stdin.readline())):
         a, b = map(int, stdin.readline().split())
         a, b = a - 1, b - 1
-        graph[a][b], graph[b][a] = True, False
-    degree = []
+        entries[a], entries[b], graph[a][b], graph[b][a] = entries[a] - 1, entries[b] + 1, True, False
+
+    zeros, queue, rank = 0, deque(), ""
+    for i in range(n):
+        if not entries[i]:
+            zeros += 1
+            queue.append(i)
+
+    if zeros > 1:
+        results.append("?")
+        continue
+
+    while queue:
+        now, zeros = queue.pop(), 0
+        for i in range(n):
+            if graph[now][i]:
+                entries[i] -= 1
+                if not entries[i]:
+                    zeros += 1
+                    queue.append(i)
+        if zeros > 1:
+            results.append("?")
+            break
+        rank += f"{now + 1} "
+    else:
+        if entries.count(0) < n:
+            results.append("IMPOSSIBLE")
+        else:
+            results.append(rank)
+
+for result in results:
+    print(result)
 
 """
 입력 예시
