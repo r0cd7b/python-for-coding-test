06569f651136bcb0a30c2a0273e9a22ac7590bc4
이상균 <tkdrbs1928@naver.com>
2022-04-07 02:18:51 +0900

    Commit.

---
+++ 14/A46.py
@@ -0,0 +1,62 @@
+# 아기 상어
+from sys import stdin
+from collections import deque
+
+
+def move():
+    if not visits[x][y] and space[x][y] <= size:
+        visits[x][y] = True
+        queue.append((x, y))
+
+
+n, space, position = int(stdin.readline()), [], None
+for i in range(n):
+    space.append(list(map(int, stdin.readline().split())))
+    for j in range(n):
+        if space[i][j] == 9:
+            position = (i, j)
+
+visits, size, queue = [[False] * n for _ in range(n)], 2, deque()
+while True:
+    x, y = position[0] - 1, 0
+    if x >= 0:
+        move()
+    x, y = position[0] + 1, 0
+    if x < n:
+        move()
+    x, y = 0, position[1] - 1
+    if y >= 0:
+        move()
+    x, y = 0, position[1] + 1
+    if y < n:
+        move()
+    if not queue:
+        break
+    position = queue.popleft()
+
+"""
+입력 예시 1
+3
+0 0 0
+0 0 0
+0 9 0
+출력 예시 1
+0
+
+입력 예시 2
+3
+0 0 1
+0 0 0
+0 9 0
+출력 예시 2
+3
+
+입력 예시 3
+4
+4 3 2 1
+0 0 0 0
+0 0 9 0
+1 2 3 4
+출력 예시 3
+14
+"""
