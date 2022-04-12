d601f939f00b0b1dbc749ce439afafdda8fd2314
이상균 <tkdrbs1928@naver.com>
2022-04-12 16:35:45 +0900

    Commit.

--- 14/A46.py
+++ 14/A46.py
@@ -1,60 +1,62 @@
 # 아기 상어
 from sys import stdin
 from copy import deepcopy
-from collections import deque
 
 
-def omnidirectional_append(x_, y_):
-    next_, distance = x_ - 1, distances[x_][y_] + 1
+def first_append(x_, y_):
+    visited_ = deepcopy(space)
+    omnidirectional_append(x_, y_, visited_, 0)
+    return visited_
+
+
+def omnidirectional_append(x_, y_, visited_, distance_):
+    next_ = x_ - 1
     if next_ >= 0:
-        append(next_, y_, distance)
+        append(visited_, next_, y_, distance_)
     next_ = x_ + 1
     if n > next_:
-        append(next_, y_, distance)
+        append(visited_, next_, y_, distance_)
     next_ = y_ - 1
     if next_ >= 0:
-        append(x_, next_, distance)
+        append(visited_, x_, next_, distance_)
     next_ = y_ + 1
     if n > next_:
-        append(x_, next_, distance)
+        append(visited_, x_, next_, distance_)
 
 
-def append(x_, y_, distance):
-    if space[x_][y_] <= size and distances[x_][y_] > distance:
-        distances[x_][y_] = distance
-        deque_.append((x_, y_))
+def append(visited_, x_, y_, distance_):
+    if size >= visited_[x_][y_] >= 0:
+        visited_[x_][y_] = -1
+        go.append((distance_ + 1, x_, y_))
 
 
 n = int(stdin.readline())
-longest: int = n ** 2
-initial = [[longest] * n for _ in range(n)]
-space, distances, deque_ = [list(map(int, stdin.readline().split())) for _ in range(n)], deepcopy(initial), None
+space, visited, size, go = [list(map(int, stdin.readline().split())) for _ in range(n)], None, 2, []
 for i in range(n):
     for j in range(n):
         if space[i][j] == 9:
-            space[i][j], distances[i][j], deque_ = 0, 0, deque([(i, j)])
+            space[i][j] = 0
+            visited = first_append(i, j)
             break
     else:
         continue
     break
-size, eaten, time = 2, 0, 0
+
+time = eaten = 0
 while True:
-    while deque_:
-        x, y = deque_.popleft()
-        omnidirectional_append(x, y)
-    shortest, x, y = longest, None, None
-    for i in range(n):
-        for j in range(n):
-            if distances[i][j] < shortest and size > space[i][j] >= 1:
-                shortest, x, y = distances[i][j], i, j
-    if shortest >= longest:
+    for distance, x, y in go:
+        omnidirectional_append(x, y, visited, distance)
+    go.sort()
+    for distance, x, y in go:
+        if size > space[x][y] >= 1:
+            space[x][y] = 0
+            go.clear()
+            visited, time, eaten = first_append(x, y), time + distance, eaten + 1
+            if size <= eaten:
+                size, eaten = size + 1, 0
+            break
+    else:
         break
-    eaten, space[x][y], time = eaten + 1, 0, distances[x][y] + time
-    if size <= eaten:
-        size, eaten = size + 1, 0
-    distances = deepcopy(initial)
-    distances[x][y] = 0
-    deque_.append((x, y))
 print(time)
 
 """
