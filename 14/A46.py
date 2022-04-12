ef626567247d0592c951594f12f0fcb755789d19
이상균 <tkdrbs1928@naver.com>
2022-04-12 18:41:02 +0900

    Commit.

--- 14/A46.py
+++ 14/A46.py
@@ -1,61 +1,65 @@
 # 아기 상어
 from sys import stdin
+from collections import deque
 from copy import deepcopy
 
 
-def first_append(x_, y_):
+def first_visit(x_, y_):
     visited_ = deepcopy(space)
-    omnidirectional_append(x_, y_, visited_, 0)
+    visited_[x_][y_] = visited_space
+    omnidirectional_append(x_, y_, 0, visited_)
     return visited_
 
 
-def omnidirectional_append(x_, y_, visited_, distance_):
+def omnidirectional_append(x_, y_, distance_, visited_):
     next_ = x_ - 1
     if next_ >= 0:
-        append(visited_, next_, y_, distance_)
+        append(next_, y_, distance_, visited_)
     next_ = x_ + 1
     if n > next_:
-        append(visited_, next_, y_, distance_)
+        append(next_, y_, distance_, visited_)
     next_ = y_ - 1
     if next_ >= 0:
-        append(visited_, x_, next_, distance_)
+        append(x_, next_, distance_, visited_)
     next_ = y_ + 1
     if n > next_:
-        append(visited_, x_, next_, distance_)
+        append(x_, next_, distance_, visited_)
 
 
-def append(visited_, x_, y_, distance_):
-    if size >= visited_[x_][y_] >= 0:
-        visited_[x_][y_] = -1
-        go.append((distance_ + 1, x_, y_))
+def append(x_, y_, distance_, visited_):
+    distance_ += 1
+    if size > visited_[x_][y_] >= 1:
+        edible.append((distance_, x_, y_))
+    if visited_[x_][y_] <= size:
+        visited_[x_][y_] = visited_space
+        bfs.append((distance_, x_, y_))
 
 
-n = int(stdin.readline())
-space, visited, size, go = [list(map(int, stdin.readline().split())) for _ in range(n)], None, 2, []
+n, visited, size = int(stdin.readline()), None, 2
+space, visited_space, edible, bfs = [list(map(int, stdin.readline().split())) for _ in range(n)], n ** 2, [], deque()
 for i in range(n):
     for j in range(n):
         if space[i][j] == 9:
             space[i][j] = 0
-            visited = first_append(i, j)
+            visited = first_visit(i, j)
             break
     else:
         continue
     break
 time = eaten = 0
 while True:
-    for distance, x, y in go:
-        omnidirectional_append(x, y, visited, distance)
-    go.sort()
-    for distance, x, y in go:
-        if size > space[x][y] >= 1:
-            space[x][y] = 0
-            go.clear()
-            visited, time, eaten = first_append(x, y), time + distance, eaten + 1
-            if size <= eaten:
-                size, eaten = size + 1, 0
-            break
-    else:
+    while bfs:
+        distance, x, y = bfs.popleft()
+        omnidirectional_append(x, y, distance, visited)
+    if not edible:
         break
+    edible.sort()
+    distance, x, y = edible[0]
+    eaten, space[x][y] = eaten + 1, 0
+    if size <= eaten:
+        size, eaten = size + 1, 0
+    edible.clear()
+    time, visited = time + distance, first_visit(x, y)
 print(time)
 
 """
