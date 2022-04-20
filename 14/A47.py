53c8302bdbd188e6c0e3dcf95de32fdfb626f8f8
이상균 <tkdrbs1928@naver.com>
2022-04-20 15:07:13 +0900

    Commit.

--- 14/A47.py
+++ 14/A47.py
@@ -1,41 +1,58 @@
 # 청소년 상어
 from sys import stdin
+from copy import deepcopy
 
-shark_fish, space = [[] for _ in range(17)], [[17] * 4 for _ in range(4)]
+
+def append(_x, _y):
+    _x, _y = _x + x_axis[fish[0][2]], _y + y_axis[fish[0][2]]
+    while 0 <= _x <= 3 >= _y >= 0:
+        stack.append((x, y))
+        _x, _y = _x + x_axis[fish[0][2]], _y + y_axis[fish[0][2]]
+
+
+fish, space = [[] for _ in range(17)], [[17] * 4 for _ in range(4)]
 for i in range(4):
-    line = list(map(int, stdin.readline().split()))
+    stdin = list(map(int, stdin.readline().split()))
     for j in range(4):
-        twice = j * 2
-        shark_fish[line[twice]].append(i)
-        shark_fish[line[twice]].append(j)
-        shark_fish[line[twice]].append(line[twice + 1] - 1)
-        space[i][j] = line[twice]
-shark_fish[0].append(0)
-shark_fish[0].append(0)
-shark_fish[0].append(shark_fish[space[0][0]][0])
-shark_fish[space[0][0]].clear()
-x, y, maximum, space[0][0] = [-1, -1, 0, 1, 1, 1, 0, -1], [0, -1, -1, -1, 0, 1, 1, 1], space[0][0], 0
+        j_2 = j * 2
+        fish[stdin[j_2]].append(i)
+        fish[stdin[j_2]].append(j)
+        fish[stdin[j_2]].append(stdin[j_2 + 1] - 1)
+        space[i][j] = stdin[j_2]
 
+fish[0].append(0)
+fish[0].append(0)
+fish[0].append(fish[space[0][0]][2])
+fish[space[0][0]].clear()
+fish[0].append(space[0][0])
+space[0][0] = 0
+
+x_axis, y_axis = [-1, -1, 0, 1, 1, 1, 0, -1], [0, -1, -1, -1, 0, 1, 1, 1]
 for i in range(1, 17):
-    if shark_fish[i]:
+    if fish[i]:
         for j in range(8):
-            next_x = shark_fish[i][0] + x[shark_fish[i][2]]
-            next_y = shark_fish[i][1] + y[shark_fish[i][2]]
-            if (shark_fish[0][0], shark_fish[0][1]) != (next_x, next_y) and 0 <= next_x <= 3 >= next_y >= 0:
-                shark_fish[space[next_x][next_y]][0], \
-                shark_fish[space[next_x][next_y]][1], \
-                space[shark_fish[i][0]][shark_fish[i][1]], \
-                shark_fish[i][0], \
-                shark_fish[i][1], \
+            next_x, next_y = fish[i][0] + x_axis[fish[i][2]], fish[i][1] + y_axis[fish[i][2]]
+            if space[next_x][next_y] and 0 <= next_x <= 3 >= next_y >= 0:
+                fish[space[next_x][next_y]][0], \
+                fish[space[next_x][next_y]][1], \
+                space[fish[i][0]][fish[i][1]], \
+                fish[i][0], \
+                fish[i][1], \
                 space[next_x][next_y] = \
-                    shark_fish[i][0], shark_fish[i][1], space[next_x][next_y], next_x, next_y, i
+                    fish[i][0], fish[i][1], space[next_x][next_y], next_x, next_y, i
                 break
-            shark_fish[i][2] = (shark_fish[i][2] + 1) % 8
+            fish[i][2] = (fish[i][2] + 1) % 8
+
+stack = []
+append(fish[0][0], fish[0][1])
+while stack:
+    x, y = stack.pop()
+    append(x, y)
 
 print()
 for s in space:
     print(s)
-print(shark_fish)
+print(fish)
 
 """
 입력 예시 1
