c66a6d7f87caa244838caae28129260b13c829b4
이상균 <tkdrbs1928@naver.com>
2022-05-04 23:30:00 +0900

    Commit.

---
+++ 14/A48-1.py
@@ -0,0 +1,6 @@
+list_ = [0, 1, 2]
+
+for i in range(len(list_)):
+    print(i)
+    del list_[-1]
+    print(list_)
--- 14/A48.py
+++ 14/A48.py
@@ -1,16 +1,6 @@
 # 어른 상어
 from sys import stdin
 
-
-def move(_x, _y):
-    grids[_x][_y].append(shark)
-    sharks[shark][2] = _x
-    sharks[shark][3] = _y
-    # 맵과, 냄새 리스트에 냄새 추가
-    # 맵에는 냄새(상어) 번호
-    # 냄새 리스트에는 좌표와 잔여 시간
-
-
 N, M, K = map(int, stdin.readline().split())
 
 grids: list[list[list[int]]] = [[[] for _ in range(N)] for _ in range(N)]
@@ -18,57 +8,70 @@ for i in range(N):
     string = stdin.readline().split()
     for j in range(N):
         if string[j] != '0':
+            grids[i][j].append(K)
             grids[i][j].append(int(string[j]))
-
 string = stdin.readline().split()
 sharks = {i + 1: [int(string[i]) - 1] for i in range(M)}
-
 for i in range(M):
     sharks[i + 1].append(tuple(tuple(int(string) - 1 for string in stdin.readline().split()) for _ in range(4)))
-
 for i in range(N):
     for j in range(N):
         if grids[i][j]:
-            sharks[grids[i][j][0]].append(i)
-            sharks[grids[i][j][0]].append(j)
+            sharks[grids[i][j][1]].append(i)
+            sharks[grids[i][j][1]].append(j)
 
 vectors = ((-1, 1, 0, 0), (0, 0, -1, 1))
-for shark in sharks:
-    go = []
+smells = []
 
-    for direction in sharks[shark][1][sharks[shark][0]]:
-        next_ = (sharks[shark][2] + vectors[0][direction], sharks[shark][3] + vectors[1][direction])
+for number in sharks:
+    go = []
+    for direction in sharks[number][1][sharks[number][0]]:
+        next_ = (sharks[number][2] + vectors[0][direction], sharks[number][3] + vectors[1][direction])
         if 0 <= next_[0] < N > next_[1] >= 0:
             go.append((next_[0], next_[1]))
 
     for x, y in go:
         if not grids[x][y]:
-            move(x, y)
+            grids[x][y].append(K)
+            grids[x][y].append(number)
+            sharks[number][2] = x
+            sharks[number][3] = y
+            smells.append((x, y))
             break
     else:
         for x, y in go:
-            if grids[x][y][0] == shark:
-                move(x, y)
+            if grids[x][y][1] == number:
+                grids[x][y][0] = K
+                sharks[number][2] = x
+                sharks[number][3] = y
                 break
 
-    for i in range(N):
-        for j in range(N):
-            if len(grids[i][j]) > 1:
-                index = 0
-                for k in range(1, len(grids[i][j])):
-                    if grids[i][j][index] > grids[i][j][k]:
-                        index = k
-                if index > 0:
-                    grids[i][j][index], grids[i][j][0] = grids[i][j][0], grids[i][j][index]
-                for k in range(-1, -len(grids[i][j]), -1):
-                    del shark[grids[i][j][k]]
-                    del grids[i][j][k]
+for i in range(N):
+    for j in range(N):
+        if len(grids[i][j]) > 2:
+            index = 1
+            for k in range(2, len(grids[i][j])):
+                if grids[i][j][index] > grids[i][j][k]:
+                    index = k
+            grids[i][j][index], grids[i][j][1] = grids[i][j][1], grids[i][j][index]
+            for k in range(2, len(grids[i][j])):
+                del sharks[grids[i][j][k]]
+            grids[i][j] = [grids[i][j][0], grids[i][j][index]]
 
-    # 냄새 잔여 시간 확인 후 삭제
+new_smells = []
+while smells:
+    smell = smells.pop()
+    if grids[smell[0]][smell[1]][0] > 1:
+        grids[smell[0]][smell[1]][0] -= 1
+        new_smells.append(smell)
+    else:
+        grids[smell[0]][smell[1]].clear()
+smells = new_smells
 
 for g in grids:
     print(g)
 print(sharks)
+print(smells)
 
 """
 입력 예시 1
