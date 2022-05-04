191566ab15fd0f9a730d2993761fafd478ee244c
이상균 <tkdrbs1928@naver.com>
2022-05-05 00:27:36 +0900

    Commit.

--- 14/A48-1.py
+++
@@ -1,6 +0,0 @@
-list_ = [0, 1, 2]
-
-for i in range(len(list_)):
-    print(i)
-    del list_[-1]
-    print(list_)
--- 14/A48.py
+++ 14/A48.py
@@ -1,77 +1,91 @@
 # 어른 상어
 from sys import stdin
 
-N, M, K = map(int, stdin.readline().split())
 
+def allocate_shark(_x, _y, _shark):
+    grids[_x][_y].append(K)
+    grids[_x][_y].append(_shark)
+    smells.append((_x, _y))
+
+
+def move_shark():
+    sharks[shark][0] = direction
+    sharks[shark][2] = x
+    sharks[shark][3] = y
+
+
+N, M, K = map(int, stdin.readline().split())
 grids: list[list[list[int]]] = [[[] for _ in range(N)] for _ in range(N)]
+smells = []
 for i in range(N):
     string = stdin.readline().split()
     for j in range(N):
         if string[j] != '0':
-            grids[i][j].append(K)
-            grids[i][j].append(int(string[j]))
+            allocate_shark(i, j, int(string[j]))
 string = stdin.readline().split()
 sharks = {i + 1: [int(string[i]) - 1] for i in range(M)}
 for i in range(M):
     sharks[i + 1].append(tuple(tuple(int(string) - 1 for string in stdin.readline().split()) for _ in range(4)))
+
 for i in range(N):
     for j in range(N):
         if grids[i][j]:
             sharks[grids[i][j][1]].append(i)
             sharks[grids[i][j][1]].append(j)
-
 vectors = ((-1, 1, 0, 0), (0, 0, -1, 1))
-smells = []
 
-for number in sharks:
-    go = []
-    for direction in sharks[number][1][sharks[number][0]]:
-        next_ = (sharks[number][2] + vectors[0][direction], sharks[number][3] + vectors[1][direction])
-        if 0 <= next_[0] < N > next_[1] >= 0:
-            go.append((next_[0], next_[1]))
+for i in range(3):
+    for g in grids:
+        print(g)
+    print(sharks)
+    print(smells)
+    print()
 
-    for x, y in go:
-        if not grids[x][y]:
-            grids[x][y].append(K)
-            grids[x][y].append(number)
-            sharks[number][2] = x
-            sharks[number][3] = y
-            smells.append((x, y))
-            break
-    else:
-        for x, y in go:
-            if grids[x][y][1] == number:
-                grids[x][y][0] = K
-                sharks[number][2] = x
-                sharks[number][3] = y
+    for shark in sharks:
+        go = []
+        for direction in sharks[shark][1][sharks[shark][0]]:
+            next_ = (sharks[shark][2] + vectors[0][direction], sharks[shark][3] + vectors[1][direction])
+            if 0 <= next_[0] < N > next_[1] >= 0:
+                go.append((direction, next_[0], next_[1]))
+        for direction, x, y in go:
+            if not grids[x][y]:
+                allocate_shark(x, y, shark)
+                move_shark()
                 break
+        else:
+            for direction, x, y in go:
+                if grids[x][y][1] == shark:
+                    grids[x][y][0] = K
+                    move_shark()
+                    break
 
-for i in range(N):
     for j in range(N):
-        if len(grids[i][j]) > 2:
-            index = 1
-            for k in range(2, len(grids[i][j])):
-                if grids[i][j][index] > grids[i][j][k]:
-                    index = k
-            grids[i][j][index], grids[i][j][1] = grids[i][j][1], grids[i][j][index]
-            for k in range(2, len(grids[i][j])):
-                del sharks[grids[i][j][k]]
-            grids[i][j] = [grids[i][j][0], grids[i][j][index]]
+        for k in range(N):
+            if len(grids[j][k]) > 2:
+                index = 1
+                for m in range(2, len(grids[j][k])):
+                    if grids[j][k][index] > grids[j][k][m]:
+                        index = m
+                grids[j][k][index], grids[j][k][1] = grids[j][k][1], grids[j][k][index]
+                for m in range(2, len(grids[j][k])):
+                    del sharks[grids[j][k][m]]
+                grids[j][k] = [grids[j][k][0], grids[j][k][index]]
 
-new_smells = []
-while smells:
-    smell = smells.pop()
-    if grids[smell[0]][smell[1]][0] > 1:
-        grids[smell[0]][smell[1]][0] -= 1
-        new_smells.append(smell)
-    else:
-        grids[smell[0]][smell[1]].clear()
-smells = new_smells
+    new_smells = []
+    while smells:
+        smell = smells.pop()
+        if grids[smell[0]][smell[1]][0] > 1:
+            grids[smell[0]][smell[1]][0] -= 1
+            new_smells.append(smell)
+        else:
+            grids[smell[0]][smell[1]].clear()
+    smells = new_smells
 
-for g in grids:
-    print(g)
-print(sharks)
-print(smells)
+    if len(sharks) < 2:
+        print(i)
+        break
+else:
+    print(-1)
 
 """
 입력 예시 1
@@ -116,4 +130,6 @@ print(smells)
 3 2 4 1
 1 4 2 3
 1 4 2 3
+출력 예시 2
+14
 """
