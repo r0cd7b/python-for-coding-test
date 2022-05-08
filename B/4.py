a9dbf5e73c1e6fbafaae553674c820ef737c5dc9
이상균 <tkdrbs1928@naver.com>
2022-05-08 20:20:35 +0900

    Commit.

--- B/4.py
+++ B/4.py
@@ -1,12 +1,12 @@
 # 투 포인터
-data, m = [1, 2, 3, 2, 5], 5
-start, end, difference, count = 0, 0, data[0] - m, 0
+m, data = 5, [1, 2, 3, 2, 5]
+start, end, difference, count = 0, 0, m - data[0], 0
 while start < len(data) > end:
     if difference == 0:
-        difference, start, count = difference - data[start], start + 1, count + 1
-    elif difference >= 0:
-        difference, start = difference - data[start], start + 1
+        difference, start, count = difference + data[start], start + 1, count + 1
     elif difference < 0:
+        difference, start = difference + data[start], start + 1
+    elif difference > 0:
         end += 1
-        difference += data[end]
+        difference -= data[end]
 print(count)
