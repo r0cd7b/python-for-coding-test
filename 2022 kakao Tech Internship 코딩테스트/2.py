26faaf870c412d33c5227dcad96eb37746fccaf3
이상균 <tkdrbs1928@naver.com>
2022-05-08 14:45:22 +0900

    Commit.

--- 2022 kakao Tech Internship 코딩테스트/2.py
+++ 2022 kakao Tech Internship 코딩테스트/2.py
@@ -2,25 +2,18 @@ from collections import deque
 
 
 def solution(queue1, queue2):
-    deque1 = deque(queue1)
-    deque2 = deque(queue2)
-    minimum = -1
-    for i in range(len(deque1)):
-        deque3 = deque(deque1)
-        deque4 = deque(deque2)
-        for j in range(len(deque2)):
-            print(deque3)
-            print(deque4)
-            print()
-            if sum(deque3) == sum(deque4):
-                number = i + j
-                if not (number >= minimum >= 0):
-                    minimum = number
-            deque3.append(deque4.popleft())
-        deque2.append(deque1.popleft())
-    return minimum
+    deque1, deque2 = deque(queue1), deque(queue2)
+    for i in range(len(deque1) + len(deque2)):
+        sum1, sum2 = sum(deque1), sum(deque2)
+        if sum1 == sum2:
+            return i
+        if sum1 < sum2:
+            deque1.append(deque2.popleft())
+        elif sum1 > sum2:
+            deque2.append(deque1.popleft())
+    return -1
 
 
-print(solution([3, 2, 7, 2], [4, 6, 5, 1]))
-print(solution([1, 2, 1, 2], [1, 10, 1, 2]))
-print(solution([1, 1], [1, 5]))
+print(solution([3, 2, 7, 2], [4, 6, 5, 1]) == 2)
+print(solution([1, 2, 1, 2], [1, 10, 1, 2]) == 7)
+print(solution([1, 1], [1, 5]) == -1)
