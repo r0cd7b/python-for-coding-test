65ebc13d23162ffb6e454365b2f1a3631a60df0f
이상균 <tkdrbs1928@naver.com>
2022-05-08 15:31:24 +0900

    Commit.

--- 2022 kakao Tech Internship 코딩테스트/2.py
+++ 2022 kakao Tech Internship 코딩테스트/2.py
@@ -2,32 +2,38 @@ from collections import deque
 
 
 def solution(queue1, queue2):
-    sum1, sum2, deque1, deque2 = sum(queue1), sum(queue2), deque(queue1), deque(queue2)
-    for i in range(len(queue1) + len(queue2)):
-        if sum1 == sum2:
-            return i
-        if sum1 < sum2:
+    difference = sum(queue1) - sum(queue2)
+    if difference == 0:
+        return 0
+    deque1, deque2 = deque(queue1), deque(queue2)
+    for i in range(1, len(deque1) + len(deque2)):
+        if difference < 0:
             deque1.append(deque2.popleft())
-            sum1, sum2 = sum1 + deque1[-1], sum2 - deque1[-1]
-        elif sum1 > sum2:
+            difference += deque1[-1] * 2
+        elif difference > 0:
             deque2.append(deque1.popleft())
-            sum1, sum2 = sum1 - deque2[-1], sum2 + deque2[-1]
+            difference -= deque2[-1] * 2
+        if difference == 0:
+            return i
     return -1
 
 
-print(solution([3, 2, 7, 2], [4, 6, 5, 1]) == 2)
-print(solution([1, 2, 1, 2], [1, 10, 1, 2]) == 7)
-print(solution([1, 1], [1, 5]) == -1)
+print(solution([3, 2, 7, 2], [4, 6, 5, 1]))  # 2
+print(solution([1, 2, 1, 2], [1, 10, 1, 2]))  # 7
+print(solution([1, 1], [1, 5]))  # -1
 
 # def solution(queue1, queue2):
-#     queue, sum1, sum2, start, end = queue1 + queue2, sum(queue1), sum(queue2), 0, len(queue1)
-#     for i in range(len(queue)):
-#         if sum1 == sum2:
-#             return i
-#         if sum1 > sum2:
-#             sum1, sum2 = sum1 - queue[start], sum2 + queue[start]
+#     difference = sum(queue1) - sum(queue2)
+#     if difference == 0:
+#         return 0
+#     queue, start, end = queue1 + queue2, 0, len(queue1)
+#     for i in range(1, len(queue)):
+#         if difference > 0:
+#             difference -= queue[start] * 2
 #             start = (start + 1) % len(queue)
-#         elif sum1 < sum2:
-#             sum1, sum2 = sum1 + queue[end], sum2 - queue[end]
+#         elif difference < 0:
+#             difference += queue[end] * 2
 #             end = (end + 1) % len(queue)
+#         if difference == 0:
+#             return i
 #     return -1
