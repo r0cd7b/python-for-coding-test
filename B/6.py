fd4ef784692450b7e062f5664055654a381afba9
이상균 <tkdrbs1928@naver.com>
2022-05-10 14:43:55 +0900

    Commit.

--- B/6.py
+++ B/6.py
@@ -1,5 +1,6 @@
 # 구간 합 계산
-data, prefix_sum = [10, 20, 30, 40, 50], [0]
+data = [10, 20, 30, 40, 50]
+prefix_sum = [0]
 for i in range(len(data)):
     prefix_sum.append(data[i] + prefix_sum[i])
 left, right = 3, 4
