c4b35f24174c4f2d7b693b80d2d8c5c8fc15e0c9
이상균 <tkdrbs1928@naver.com>
2022-05-22 15:34:35 +0900

    Commit.

--- 2021 카카오 인턴십 for Tech developers 코딩 테스트/1.py
+++
@@ -1,7 +0,0 @@
-from sys import stdin
-
-s = stdin.readline().rstrip()
-english = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-for i in range(len(english)):
-    s = s.replace(english[i], str(i))
-print(s)
