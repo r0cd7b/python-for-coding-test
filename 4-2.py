d389593e9b4016725e7f3633d87ca4c38a852db6
r0cd7b <tkdrbs1928@naver.com>
2023-05-24 18:00:00 +0900

    commit

--- 4-2.py
+++ 4-2.py
@@ -7,11 +7,8 @@ hours = 0
 for hour in range(n):
     if hour % 10 == 3:
         hours += 1
-# minutes_seconds = 0
-# for minute_second in range(60):
-#     if minute_second // 10 == 3 or minute_second % 10 == 3:
-#         minutes_seconds += 1
-cases = n * 1575 + hours * 2025  # n * 60 * 60 - (n - hours) * (60 - minutes_seconds) * (60 - minutes_seconds)
+# minutes_seconds = 15 = 6 * 10 - (6 - 1) * (10 - 1)
+cases = n * 1575 + hours * 2025  # = n * 60 * 60 - (n - hours) * (60 - minutes_seconds) * (60 - minutes_seconds)
 
 print(cases)
 '''
---
+++ 4-3.py
@@ -0,0 +1,7 @@
+#
+import sys
+
+sys.stdin.readline()
+print()
+'''
+'''
